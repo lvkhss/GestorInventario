91db114518b70c6f420012177fc62e1dd36e30d4
IanHazecure <136367549+IanHazecure@users.noreply.github.com>
2025-07-01 03:13:02 -0400

    test to docker

--- inventory_management_system/settings.py
+++ inventory_management_system/settings.py
@@ -25,7 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'inventory',
+    'inventory.apps.InventoryConfig',
     'import_export',
     'django.contrib.humanize',
 ]
